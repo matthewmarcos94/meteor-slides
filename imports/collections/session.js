18ef2a8dfb0d231afc07e201aa4639f8c8fe5f58
Matthew Marcos <matthewmarcos94@gmail.com>
2016-05-13 13:43:43 +0800

    Edited slides for release

--- imports/collections/session.js
+++ imports/collections/session.js
@@ -1,11 +1,11 @@
 import { Mongo } from 'meteor/mongo';
- 
+
 export const Session = new Mongo.Collection('session');
 
 export const SessionActions = {
     'decrement'() {
         let session = Session.findOne();
-        
+
         if(session.index > 0) {
             Session.update({_id: session._id}, {
                 $inc: {
@@ -16,8 +16,8 @@ export const SessionActions = {
     },
     'increment'() {
         let session = Session.findOne();
-        
-        if(session.index < 24) {
+
+        if(session.index < 4) {
             Session.update({_id: session._id}, {
                 $inc: {
                     index: 1
